f878689fb37db5e1f74db817a53fd0c74141e337
piyaladamas2 <piyal.adamas2@gmail.com>
2025-03-26 19:07:53 +0530

    First set up

---
+++ src/main/java/com/piyal/Digital/Library/Book/Management/DigitalLibraryBookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.piyal.Digital.Library.Book.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLibraryBookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalLibraryBookManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piyal/Digital/Library/Book/Management/DigitalLibraryBookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyal.Digital.Library.Book.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibraryBookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
