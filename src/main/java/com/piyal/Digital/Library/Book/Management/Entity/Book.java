5764d2ea1f6ff4eb57041ab5da948131ccfa3f03
piyaladamas2 <piyal.adamas2@gmail.com>
2025-03-26 22:34:12 +0530

    final commit

--- src/main/java/com/piyal/Digital/Library/Book/Management/Entity/Book.java
+++ src/main/java/com/piyal/Digital/Library/Book/Management/Entity/Book.java
@@ -1,26 +1,18 @@
 package com.piyal.Digital.Library.Book.Management.Entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class Book {
 
-  @Id
-  @GeneratedValue
   private long id;
 
   @NotEmpty(message = "Title cannot be empty")
